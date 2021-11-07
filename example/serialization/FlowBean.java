81a58cd0118741cb0e5ff4d4cb440ead917fc656
Tommy Vercetti <74338779+2402575933@users.noreply.github.com>
2021-11-07 18:03:13 +0800

    FlowBean

---
+++ example/serialization/FlowBean.java
@@ -0,0 +1,63 @@
+package com.tommy.writable;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlowBean implements Writable {
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    // 空参构造
+    public FlowBean() {
+
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow() {
+        this.sumFlow = this.upFlow + this.downFlow;
+    }
+
+    // 序列化
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(upFlow);
+        dataOutput.writeLong(downFlow);
+        dataOutput.writeLong(sumFlow);
+    }
+    
+    // 反序列化
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.upFlow = dataInput.readLong();
+        this.downFlow = dataInput.readLong();
+        this.sumFlow = dataInput.readLong();
+    }
+    
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+}
